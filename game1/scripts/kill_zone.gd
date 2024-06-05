3d252bcb5b49ff556974e2ec7ed39a3cc93ba41f
luke Slater <luke@lukes-MacBook-Pro.local>
2024-06-05 13:44:34 +0200

    fixed some errors

--- game1/scripts/kill_zone.gd
+++ game1/scripts/kill_zone.gd
@@ -4,9 +4,12 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("you died")
+	Engine.time_scale = 0.5#slows down overwall game time
+	body.get_node(("CollisionShape2D")).queue_free()#removes de colldier of the player
 	timer.start()
 #triggers the killzone 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
 # restarts the scene
